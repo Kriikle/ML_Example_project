73bd14b1459dbbf9fb6c1762cc3821210b2b89af
Kirill <61191094+Karen-art-tech@users.noreply.github.com>
2022-05-08 00:35:46 +0300

    Adding pandas profiling
    -Structure has been chaned to scc/Forest_ml
    -Added profiling report
    -Poety dependeses added

---
+++ src/Forest_ml/eda.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pandas_profiling
+
+def create_profile_report():
+    """
+    Crating report with pandas profile
+    File name is train.csv it must be in data folder
+    """
+    df = pd.read_csv('../../data/train.csv')
+    profile = df.profile_report()
+    profile.to_file("../../report/pandas_profiling.html")
+
+
+
