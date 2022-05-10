cab000b6670253c9fecabad9f9a08e3305523ba7
Kirill <61191094+Karen-art-tech@users.noreply.github.com>
2022-05-10 23:46:25 +0300

    Adding two models to train
    Just base pipeline without k-folds and cross-validation

--- src/Forest_ml/train.py
+++ src/Forest_ml/train.py
@@ -1,3 +1,4 @@
+from ast import Str
 from pathlib import Path
 from joblib import dump
 
@@ -14,11 +15,14 @@ from .pipeline import create_pipeline
 parser = argparse.ArgumentParser(description='Program parasms')
 
 #file path
-parser.add_argument('-data_path', type=Path,default="data/train.csv")
-parser.add_argument('-finalfile_path', type=Path,default="data/model.joblib")
+parser.add_argument('-dataset_path', type=Path,default="data/train.csv")
+parser.add_argument('-save_model_path', type=Path,default="data/model.joblib")
 
 #Model select
 parser.add_argument('-model_choise',type=int,default=0)
+
+
+parser.add_argument('-use_scaler',type=bool,default=True)
 
 #First model
 parser.add_argument('-max_iter', type=int,default=100)
@@ -29,7 +33,7 @@ parser.add_argument('-random_state', type=int,default= 42)
 #Second model
 
 parser.add_argument('-n_estimators', type=int,default=100)
-parser.add_argument('-criterion', type=ascii,default="gini")
+parser.add_argument('-criterion', type=str,default='gini',choices=['gini', 'entropy'])
 parser.add_argument('-max_depth', type=int,default=None)
 
 args = parser.parse_args()
@@ -37,26 +41,49 @@ print(args)
 
 
 def train(
-    dataset_path: Path,
-    save_model_path: Path,
-    random_state: int,
-    test_split_ratio: float,
-    use_scaler: bool,
-    max_iter: int,
-    logreg_c: float,
-) -> None:
+    dataset_path = args.dataset_path ,
+    save_model_path= args.save_model_path,
+    model_num = args.model_choise,
+    random_state = args.random_state,
+    test_split_ratio = 0.2,
+    use_scaler = args.use_scaler,
+    max_iter = args.max_iter,
+    logreg_c = args.logreg_C,
+    n_estimators = args.n_estimators,
+    criterion =args.criterion,
+    max_depth = args.max_depth,
+    target = "Cover_Type"
+):
     features_train, features_val, target_train, target_val = get_dataset(
         dataset_path,
         random_state,
         test_split_ratio,
+        target,
     )
     with mlflow.start_run():
-        pipeline = create_pipeline(use_scaler, max_iter, logreg_c, random_state)
+        pipeline = create_pipeline(
+            model_num = model_num,
+            use_scaler = use_scaler,
+            max_iter = max_iter,
+            logreg_C = logreg_c,
+            random_state = random_state,
+            n_estimators = n_estimators,
+            criterion = criterion,
+            max_depth = max_depth
+        )
         pipeline.fit(features_train, target_train)
         accuracy = accuracy_score(target_val, pipeline.predict(features_val))
-        mlflow.log_param("use_scaler", use_scaler)
-        mlflow.log_param("max_iter", max_iter)
-        mlflow.log_param("logreg_c", logreg_c)
+        if model_num == 0:
+            mlflow.log_param("Models", "LogisticRegression")
+            mlflow.log_param("use_scaler", use_scaler)
+            mlflow.log_param("max_iter", max_iter)
+            mlflow.log_param("logreg_c", logreg_c)
+        else:
+            mlflow.log_param("Models", "RandomForestClassifier")
+            mlflow.log_param("n_estimators", n_estimators)
+            mlflow.log_param("criterion", criterion)
+            mlflow.log_param("max_depth", max_depth)
+
         mlflow.log_metric("accuracy", accuracy)
         #click.echo(f"Accuracy: {accuracy}.")
         dump(pipeline, save_model_path)
