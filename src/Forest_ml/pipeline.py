951821f6b70f812819b75a1c47ce53af15c1719c
Kirill <61191094+Karen-art-tech@users.noreply.github.com>
2022-05-10 21:56:09 +0300

    Making pipline

---
+++ src/Forest_ml/pipeline.py
@@ -0,0 +1,31 @@
+from sklearn.linear_model import LogisticRegression,RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+def create_pipeline(
+    use_scaler: bool, max_iter: int, logreg_C: float, random_state: int,
+    model_num: int,n_estimators: int,criterion: str,max_depth: int
+) -> Pipeline:
+    pipeline_steps = []
+    if use_scaler:
+        pipeline_steps.append(("scaler", StandardScaler()))
+    if model_num == 0:
+        pipeline_steps.append(
+            (
+                "classifier",
+                LogisticRegression(
+                    random_state=random_state, max_iter=max_iter, C=logreg_C
+                ),
+            )
+        )
+    else:
+         pipeline_steps.append(
+            (
+                "classifier",
+                RandomForestClassifier(
+                    n_estimators=n_estimators,criterion=criterion,max_depth=max_depth
+                ),
+            )
+        )
+    return Pipeline(steps=pipeline_steps)
