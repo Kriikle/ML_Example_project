9da3e8758fc51de6fefa5d8c5213eec98693de31
Kirill <61191094+Karen-art-tech@users.noreply.github.com>
2022-05-10 22:57:03 +0300

    Adding to poetry some scripts path

--- src/Forest_ml/reports.py
+++ src/Forest_ml/reports.py
@@ -6,9 +6,9 @@ def create_profile_report():
     Crating report with pandas profile
     File name is train.csv it must be in data folder
     """
-    df = pd.read_csv('../../data/train.csv')
+    df = pd.read_csv('data/train.csv')
     profile = df.profile_report()
-    profile.to_file("../../report/pandas_profiling.html")
+    profile.to_file("report/pandas_profiling.html")
 
 
 
